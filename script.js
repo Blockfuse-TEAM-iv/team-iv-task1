e7d375ff0026141b2ca51895956c163dcc710c5f
retkatmun <retkatmun1@gmail.com>
2026-03-23 14:54:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+/* by miracale
+1. Store a quote and the author in variables and print:
+
+"Your quote here" - Author Name
+*/
+
+/* by john
+2. Create a user biography builder using the following variables:
+- firstName
+- lastName
+- country
+- job
+
+Output:
+"Andrew is a developer from India."
+*/
+
+/*
+by temi & scholar
+3. Create a simple user profile card generator
+
+Create multiple variables and display a formatted block:
+
+Example:
+
+Name: Solex
+Age: 34
+Country: Nigeria
+Hobby: Coding
+*/
